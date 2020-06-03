af59a2aaf907c8befda1647f829b0c0ece1f9823
mardeureu <siixx@hanmail.net>
2020-06-03 14:42:17 +0900

    m

--- asset/js/swiper.js
+++ asset/js/swiper.js
@@ -1,38 +1,87 @@
 window.addEventListener('DOMContentLoaded',function(){
-    var swiper = new Swiper('.swiper-container', {
-        direction: 'horizontal',
-        slidesPerView: 1,
-        spaceBetween: '50%',
-        mousewheel: true,
-        loop: true,
-        slidesOffsetAfter: 0,
-        watchSlidesProgress: true,
-        autoplay: {
-            delay: 5000,
-            waitForTransition: true,
-        },
-        pagination: {
-            el: '.swiper-pagination',
-            type: 'fraction',
-        },
-        
-    });
-    swiper.on('slideChangeTransitionEnd', function () {
-        loader();        
-    });
-
-    function loader(){
-        var loadNum = 1,load;
-        load = setInterval(function(){
+    function swiper(){
+       
            
-            $('.loadbar').css({
-                width:loadNum+'%'
-            });
-            if (loadNum != 100) { loadNum++ }
-            else { clearInterval(load); loadNum = 1};
-        },50)
+        var swiper = new Swiper('.swiper-container', {
+            direction: 'horizontal',
+            slidesPerView: 1,
+            spaceBetween: '50%',
+            mousewheel: true,
+            loop: true,
+            slidesOffsetAfter: 0,
+            watchSlidesProgress: true,
+            autoplay: {
+                delay: 5000,
+                waitForTransition: true,
+            },
+            pagination: {
+                el: '.swiper-pagination',
+                type: 'fraction',
+            },
+            
+        });
+        swiper.on('slideChangeTransitionStart', function () {
+            if(swiper.autoplay.running){
+                loader();
+            }else{
+                $('.loadbar').css({
+                    width:'0%'
+                });
+            }       
+            
+        });
+
+        function loader(){
+            var loadNum = 1,load;
+           
+            load = setInterval(function(){            
+                $('.loadbar').css({
+                    width:loadNum+'%'
+                });
+                if (loadNum != 100) { loadNum++ }
+                else { clearInterval(load); loadNum = 1};
+            },50)
+        }
+        loader();
     }
-    loader();
+
+    function workList(){
+        var listTag='';
+        $.ajax({
+            url : '../ajax/work.json',
+            type:'get',
+            success:function(data){
+                for(var i=0;i<data.work.length;i++){
+                    
+                    listTag = "<div class='swiper-slide'>";
+                    listTag += "<h5 data-swiper-parallax='-100'>"+data.work[i].title+"</h5>";
+                    listTag += "<a href='sub/work_sub.html#"+i+"' data-swiper-parallax='-300' class='swiper-zoom-container'>";
+                    listTag += "<img src='"+data.work[i].img+"' alt='정선희 포트폴리오 정식품 웹사이트 메인 이미지 클릭시 상세페이지로 이동됩니다.'>";
+                    listTag += "</a>";
+                    listTag += "</div>";
+                    $('.swiper-wrapper').append(listTag);
+                }
+                setTimeout(function(){swiper();},100);
+            }
+        });
+    }workList();
+
+
+    if(localStorage.logo == 'on'){
+        $('.home_logo').addClass('active');
+    }
+
+    $('.home_logo').on('click',function(e){
+        e.preventDefault();
+        if(localStorage.logo == 'on'){
+            localStorage.logo = 'off';
+        }else{
+            localStorage.logo = 'on';
+        }
+        setTimeout(function(){ location.href = $('.home_logo').attr('href'),500});
+        
+    })
+   
     
 //load END
 });
--- asset/js/work_sub.js
+++ asset/js/work_sub.js
@@ -1,11 +1,83 @@
-window.addEventListener('DomContentLoaded', function(){
-    window.onscroll = function() {progressScroll()};
-    function progressScroll() {
-        var winScroll = docuemnt.body.scrollTop || document.documentElement.scrollTop;
-        var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-        var scrolled = (winScroll / height) * 100;
-        var bar = document.querySelector('bar');
-        bar.style.width = scrolled + "%";
-    }
+window.addEventListener('DOMContentLoaded', function(){
+    // window.onscroll = function() {progressScroll()};
+    // function progressScroll() {
+    //     var winScroll = docuemnt.body.scrollTop || document.documentElement.scrollTop;
+    //     var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    //     var scrolled = (winScroll / height) * 100;
+    //     var bar = document.querySelector('bar');
+    //     bar.style.width = scrolled + "%";
+    // }
+
+    //ajax
+    function detail(){
+        var listTag='';
+        $.ajax({
+            url : '../../ajax/worksub.json',
+            type:'get',
+            success:function(data){
+                var dIdx = location.hash.slice(1);
+
+                function pageContents(){
+                   
+                    var getData='', selector=data.worksub[dIdx];
+
+                    title = selector.title;
+                    project =  selector.project;
+                    sitemap =  dataLoop(selector.sitemap,0,1);
+                    breakpoints = dataLoop(selector.breakpoints,0,1);
+                    pofolmain = "<img src='"+selector.gallery[0]+"' alt='"+selector.galleryalt[0]+"'>";
+                    problems = dataLoop(selector.problemsdt,selector.problemsdd,2);
+                    suggestion = dataLoop(selector.suggestiondt,selector.suggestiondd,2);
+                    development =  selector.development;
+                    link = selector.link;
+
+                    gallery = function(){
+                        getData="";
+                        for(var i=1;i<selector.gallery.length;i++){
+                            getData += "<img src='"+selector.gallery[i]+"' alt='"+selector.galleryalt[i]+"'>";
+                        }
+                        return getData;
+                    };
+
+                    $('h3').html(title);
+                    $('.project').html(project);
+                    $('.sitemap').html(sitemap);
+                    $('.breakpoints').html(breakpoints);
+                    $('.pofolmain').html(pofolmain);
+                    $('.problems dl').html(problems);
+                    $('.suggestion dl').html(suggestion);
+                    $('.gallery').html(gallery);
+                    $('.development').html(development);
+                    $('.portfoliolink').attr('href',link);
+
+                    $(window).scrollTop(0);
+                }pageContents();
+
+                function dataLoop(k,m,n){
+                    getData='';
+                    if (n == 1) {
+                        for (var i=0; i<k.length; i++) {
+                            getData += "<li>"+k[i]+"</li>";
+                        }
+                    } else {
+                        for (var i=0; i<k.length; i++) {
+                            getData += "<dt>"+k[i]+"</dt><dd>"+m[i]+"</dd>";
+                        }
+                    }
+                    return getData;
+                }
+                //event
+                $('.sub_btn a').on('click',function(e){
+                    e.preventDefault();
+                    if($(this).index()){
+                        dIdx != data.worksub.length-1 ? dIdx++:'';
+                    }else{
+                        dIdx != 0 ? dIdx--:'';
+                    }
+                    pageContents();
+                })
+            }
+        });
+    }detail();
 
 });
